d8dbfe52765617bdab8443fe4cf617a9721fac53
Chariselyasa123 <ahmad.charis@raharja.info>
2022-06-19 23:11:20 +0700

    make name

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,12 @@
 import '../styles/Title.css'
+import {name} from '../fetures/name'
 
 const Title = () => {
+
     return (
         <div className="title">
             <h1 className="text-4xl">Todo List App 📝</h1>
-            <i className="credit text-sm">By: Ahmad Charis Elyasa Hafidianto</i>
+            <i className="credit text-sm">By: {name}</i>
         </div>
     )
 }
--- src/components/partials/Footer.js
+++ src/components/partials/Footer.js
@@ -1,10 +1,11 @@
 import '../../styles/partials/Footer.css'
+import {name} from '../../fetures/name'
 
 const Footer = () => {
     return (
         <div className="footer">
             <h4>Todo List App</h4>
-            <i className="text-sm">By: Ahmad Charis Elyasa Hafidianto</i>
+            <i className="text-sm">By: {name}</i>
         </div>
     )
 }
